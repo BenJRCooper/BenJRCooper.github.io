b7af5984249fb9002222c4877d8f036ec493c1a3
Ben Cooper <BenJRCooper@gmail.com>
2021-02-07 14:28:43 +0000

    game grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ben';
+
+  gameSize = 8;
+  xs = Array.from(Array(this.gameSize));
+  ys = Array.from(Array(this.gameSize));
+
+  ngOnInit() {
+    console.log(this.xs, this.ys);
+  }
 }
