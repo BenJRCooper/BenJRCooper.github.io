e21a9acd2748969ab74b0fb178182e37e0364eba
Ben Cooper <BenJRCooper@gmail.com>
2021-02-07 17:07:16 +0000

    robots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+
+interface Point {
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,13 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'ben';
-
   gameSize = 8;
   xs = Array.from(Array(this.gameSize));
   ys = Array.from(Array(this.gameSize));
 
+  player1: Point = {
+    x: 0,
+    y: 2,
+  };
+  player2: Point = {
+    x: 7,
+    y: 2,
+  };
+  shot1: Point = {
+    x: 1,
+    y: 1,
+  };
+
   ngOnInit() {
     console.log(this.xs, this.ys);
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    console.log(event);
+    switch (event.code) {
+      case 'ArrowUp':
+        this.player1.y = Math.max(0, this.player1.y - 1);
+        break;
+      case 'ArrowDown':
+        this.player1.y = Math.min(this.player1.y + 1, this.gameSize - 1);
+        break;
+      case 'ArrowLeft':
+        this.player1.x = Math.max(0, this.player1.x - 1);
+        break;
+      case 'ArrowRight':
+        this.player1.x = Math.min(this.player1.x + 1, this.gameSize - 1);
+        break;
+    }
   }
 }
